b34d1b76bab12439d9bbafa187932d44b15ef59a
Noshiro Nanai <epm.maya@gmail.com>
2021-01-04 16:40:03 +0900

    Bump up v2.3.0

--- root/build.sh
+++ root/build.sh
@@ -9,7 +9,7 @@ pushd caddy
 
 go mod init caddy
 
-go get -d -v github.com/caddyserver/caddy/v2@v2.2.3
+go get -d -v github.com/caddyserver/caddy/v2@v2.3.0
 
 CGO_ENABLED=0 go build -v -ldflags "-w -s" -trimpath
 
